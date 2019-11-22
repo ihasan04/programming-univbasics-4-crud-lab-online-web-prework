1ff0209f61554f91cd754a62d60c7549440d7ed3
Ismael Hasan <ismael.hasan@live.com>
2019-11-22 20:18:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -48,5 +48,4 @@ def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number = 3
   array[index_number] = "totally"
-  p array
 end
